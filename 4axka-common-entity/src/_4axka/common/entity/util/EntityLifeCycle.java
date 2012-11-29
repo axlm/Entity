1afba8e7d26d74b588c78a0f82e0bc55b1a1f36e
Axl Mattheus <axl.mattheus@gmail.com>
2012-11-29 07:11:53 +0200

    Added life cycle events.

--- 4axka-common-entity/src/_4axka/common/entity/util/EntityLifeCycle.java
+++ 4axka-common-entity/src/_4axka/common/entity/util/EntityLifeCycle.java
@@ -16,21 +16,80 @@ package _4axka.common.entity.util;
 
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Embeddable;
 
 
 @Embeddable
 public final class EntityLifeCycle {
-    private LifeCycleEventType __type;
+    private LifeCycleStateType __state;
     private String __source;
     private Date __timestamp;
 
-    public enum LifeCycleEventType {
-        CREATE,
-        DESTROY,
-        MODIFY,
-        ACTIVATE,
-        DEACTIVATE;
+    public EntityLifeCycle() {
+        super();
+    }
+
+    public EntityLifeCycle(final LifeCycleStateType state, final String source, final Date timestamp) {
+        this();
+        __state = state;
+        __source = source;
+        __timestamp = timestamp;
+    }
+
+    public LifeCycleStateType getState() {
+        return __state;
+    }
+
+    public void setState(LifeCycleStateType state) {
+        __state = state;
+    }
+
+    public String getSource() {
+        return __source;
+    }
+
+    public void setSource(String source) {
+        __source = source;
+    }
+
+    public Date getTimestamp() {
+        return __timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        __timestamp = timestamp;
+    }
+
+    public enum LifeCycleStateType {
+        CREATED(0, 1, 2, 4),
+        DESTROYED(1),
+        MODIFIED(2, 2, 4),
+        ACTIVATED(3, 1, 2, 4),
+        DEACTIVATED(4, 3, 1);
+
+        private final int __state;
+        private final Set<Integer> __nextStates = new HashSet<>();
+
+        private LifeCycleStateType(final int state, final int... nextEvents) {
+            __state = state;
+            for (final int event_ : nextEvents) {
+                __nextStates.add(event_);
+            }
+        }
+
+        public Set<LifeCycleStateType> nextStates() {
+            final HashSet<LifeCycleStateType> result_ = new HashSet<>();
+
+            for (final LifeCycleStateType state_ : LifeCycleStateType.values()) {
+                if (state_.__nextStates.contains(__state)) {
+                    result_.add(state_);
+                }
+            }
+
+            return result_;
+        }
     }
 }
